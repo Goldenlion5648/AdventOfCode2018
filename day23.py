ac8b7a67d0e28f5f3c4ea8d43e1aa96c05c7d9b1
Goldenlion5648 <coboudinot@gmail.com>
2020-06-27 13:43:55 -0400

    manual solver code

--- day23.py
+++ day23.py
@@ -6,13 +6,24 @@ def manhat(a, b):
         total += abs(a[i] - b[i])
     return total
 
+def pointsInRange(pos, a):
+    value = 0
+    for i in a:
+        # print(pos, i)
+        if manhat(pos, i[0]) <= i[1]:
+            value += 1
+    # print(value)
+    return value
 a = []
+a = deque()
 
 for i in range(1000):
     b = input()
     a.append((list(map(int, b[5:b.index('>')]
         .split(','))), int(b[b.index('r=')+2:])))
-a.sort(key=lambda x: x[0][0])
+# a.sort(key=lambda x: x[1])
+
+# potentials = {k: v for k, v in sorted(potentials.items(), key=lambda item: item[1])}
 # for i in a:
 #     print(i)
 
@@ -21,39 +32,139 @@ a.sort(key=lambda x: x[0][0])
 #         for z in range(-100000, 100000, 10000):
 #             potentials.append([(x, y, z), 0])
 
-potentials = dict()
-
-for p in a:
-    # for i in potentials:
-    for j in range(3):
-        p[]
-    potentials[p]
-temp = list(potentials)
-temp.sort(key=lambda x: x[1])
-# print(temp)
-
-print(potentials[0][1])
-print(potentials[-1][1])
-print(len(potentials))
-pos = 0
-while pos < len(potentials):
-    if potentials[pos][1] < 101:
-        del potentials[pos]
-    else:
-        pos += 1
-# 
-# print(potentials.popleft())
-# print(potentials[0][1])
-# print(potentials[-1][1])
-# print(len(potentials))
-# print(potentials)
+points = dict()
+#12707570 31777570 16177860
+for x in range(12707470, 12707770, 10):
+    for y in range(31777270, 31777870, 10):
+        for z in range(16177560, 16178160, 10):
+            points[(x,y,z)] = 0
+# x = 14680000
+# y = 33400000
+# z = 17782000
+         
 
 
-# largest = a[-1]
-# radius = largest[1]
-# x,y, z = largest[0]
-# total = 0
+
+# dim = 200000000
+# step = 10000000
+# for z in range(-dim, dim, step):
+#     for y in range(-dim, dim, step):
+#         for x in range(-dim, dim, step):
+#             points[(x, y, z)] = 0
+#     print(z)
+# # points = dict.fromkeys([[(y, x))
+# # potentials = dict()
+
+cur = 0
+#12730000 31800000 16190000
+# newX = 12730000
+# newY = 31800000
+# newZ = 16190000
+newX,newY, newZ = [12707570, 31777570, 16177860]
+maxxed = [False,False,False]
+subtractAmount = 100
+highestNum = 913
+while maxxed != [True, True, True]:
+    if maxxed[0] == False:
+        newX -= subtractAmount 
+    if pointsInRange((newX, newY, newZ), a) < highestNum:
+        maxxed[0] = True
+        newX += subtractAmount
+        while pointsInRange((newX, newY, newZ), a) < highestNum:
+            newX -= 1
+    if maxxed[1] == False:
+        newY -= subtractAmount 
+    if pointsInRange((newX, newY, newZ), a) < highestNum:
+        maxxed[1] = True
+        newY += subtractAmount 
+        while pointsInRange((newX, newY, newZ), a) < highestNum:
+            newY -= 1
+    if maxxed[2] == False:
+        newZ -= subtractAmount 
+    if pointsInRange((newX, newY, newZ), a) < highestNum:
+        newZ += subtractAmount 
+        maxxed[2] = True
+        while pointsInRange((newX, newY, newZ), a) < highestNum:
+            newZ -= 1
+    print(maxxed)
+    print(newX, newY, newZ)
+    if pointsInRange((newX, newY, newZ), a) > highestNum:
+        print('found new', pointsInRange((newX, newY, newZ), a))
+        break
 # for i in a:
-#     if abs(i[0][0] - x) + abs(i[0][1] - y) + abs(i[0][2] - z) <= radius:
-#         total += 1
-# print(total)
+    
+    # if cur % 10 == 0:
+    #     print(cur)
+    # cur += 1
+
+# points = {k: v for k, v in sorted(points.items(), key=lambda item: item[1])}
+
+# for i in points:
+#     if points[i] >= 873:
+#         print(i, points[i])
+
+# # for p in a:
+# #     # for i in potentials:
+# #     x, y, z = p[0]
+# #     rad = p[1]
+# #     pos = (x-rad,y,z)
+
+# #     if pos not in potentials:
+# #         potentials[pos] = 1
+# #     else:
+# #         potentials[pos] += 1
+# #     pos = (x+rad,y,z)
+
+# #     if pos not in potentials:
+# #         potentials[pos] = 1
+# #     else:
+# #         potentials[pos] += 1
+# #     pos = (x,y-rad,z)
+# #     if pos not in potentials:
+# #         potentials[pos] = 1
+# #     else:
+# #         potentials[pos] += 1
+# #     pos = (x,y+rad,z)
+# #     if pos not in potentials:
+# #         potentials[pos] = 1
+# #     else:
+# #         potentials[pos] += 1
+# #     pos = (x,y,z+rad)
+# #     if pos not in potentials:
+# #         potentials[pos] = 1
+# #     else:
+# #         potentials[pos] += 1
+# #     pos = (x,y,z-rad)
+# #     if pos not in potentials:
+# #         potentials[pos] = 1
+# #     else:
+# #         potentials[pos] += 1
+
+# # potentials = {k: v for k, v in sorted(potentials.items(), key=lambda item: item[1])}
+# # print(temp)
+# # print(potentials)
+# # print(potentials[0][1])
+# # print(potentials[-1][1])
+# # print(len(potentials))
+# # pos = 0
+# # while pos < len(potentials):
+# #     if potentials[pos][1] < 101:
+# #         del potentials[pos]
+# #     else:
+# #         pos += 1
+# # 
+# # print(potentials.popleft())
+# # print(potentials[0][1])
+# # print(potentials[-1][1])
+# # print(len(potentials))
+# # print(potentials)
+
+
+# # largest = a[-1]
+# # radius = largest[1]
+# # x,y, z = largest[0]
+# # total = 0
+# # for i in a:
+# #     if abs(i[0][0] - x) + abs(i[0][1] - y) + abs(i[0][2] - z) <= radius:
+# #         total += 1
+# # print(total)
--- day6.py
+++ day6.py
@@ -7,7 +7,7 @@ def manhat(a, b):
     return total
 # board = deque([deque([0 for i in range(-30, 380)]) for j in range(-30, 380)])
 offset = 0
-dim = 404
+dim = 370
 board = deque([deque([0 for i in range(0- offset, dim)]) for j in range(0- offset, dim)])
 # board[5][6] = 7
 # for i in board:
@@ -19,31 +19,33 @@ for i in range(50):
     # c.append(tuple(map(int, input().split(','))))
     points[tuple(map(int, input().split(',')))] = 0
 
+coordCount = 0
 edgePoints = set()
 for y in range(offset, len(board)):
     for x in range(offset, len(board[0])):
         isTie = False
         closet = (1000000, 1000000)
+        curSum = 0
         for key in points:
             man1 = manhat(closet, (y,x))
             man2 = manhat(key, (y,x))
             # print(y, x, man1, man2)
-
+            curSum += man2
             if man2 < man1:
                 closet = key
                 # print('new closet', closet)
                 isTie = False
             elif man1 == man2:
                 isTie = True
-        if isTie == False:
-            # print('adding', y, x, key )
-            # print(points)
-            board[y][x] = closet
-            points[closet] += 1
-            # if x == 0 or y == 0 or x == len(board)-1 or y == len(board)-1:
-            #     points.pop(closet)
-    print(y)
+        if curSum < 10000:
+            coordCount += 1
+        # if isTie == False:
+        #     # print('adding', y, x, key )
+        #     # print(points)
+        #     board[y][x] = closet
+        #     points[closet] += 1
 
+    print(y)
 points = {k: v for k, v in sorted(points.items(), key=lambda item: item[1])}
 print(points)
 for i in board[0]:
@@ -70,67 +72,4 @@ for i in points:
 answer = {k: v for k, v in sorted(answer.items(), key=lambda item: item[1])}
 
 print(answer)
-
-
-# dist = 10
-# for p in points:
-#     for x in range(-dist, dist):
-#         for y in range(-dist, dist):
-#             if p[1] + y < 0 or p[0] + x < 0 or p[1] + y >= len(board) or \
-#             p[0] + x >= len(board[0]):
-#                 continue
-#             if board[p[1]+y][p[0]+x] == 0:
-#                 board[p[1]+y][p[0]+x] = p
-#                 points[p] += 1
-#             elif manhat(board[p[1]+y][p[0]+x], p) == manhat(board[board[p[1]][p[0]][1]][board[board[p[1]][p[0]][0]]], board[p[1]+y][p[0]+x]):
-#                 points[board[board[p[1]+y][p[0]+x]]] -= 1
-
-#             elif manhat(board[p[1]+y][p[0]+x], p) < manhat(board[p[1]][p[0]], board[p[1]+y][p[0]+x]):
-#                 # print(board[p[1]+y][p[0]+x])
-#                 points[board[p[1]+y][p[0]+x]] -= 1
-#                 points[p] += 1
-#                 board[p[1]+y][p[0]+x] = p
-
-
-
-
-
-
-# xPoints = {}
-# c.sort()
-# for i in c:
-#     xPoints[(i)] = 1
-
-# c.sort(key = lambda x: x[0]+x[1])
-# for i in c:
-#     print(i)
-
-# yPoints = {}
-# for i in c:
-#     yPoints[(i)] = 1
-# print(c)
-# print(xPoints)
-# print()
-# print(yPoints)
-
-
-
-
-# # for k in c:
-# #     points[k] = 0
-# # print(points)
-
-# count = 0
-# for k in c:
-#     board[k[1]][k[0]] = g[count]
-#     count += 1
-#     # x[k[0]] = 0
-#     # y[k[1]] = 0
-
-# print(board)
-
-# # print(x)
-# # print(y)
-# # c.sort()
-# # print(*c)
-
+print(coordCount)
